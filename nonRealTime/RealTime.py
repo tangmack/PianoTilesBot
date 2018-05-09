3d65d9979dda92a472916aab996c7ec6026b15d0
macktang <macktang@gmail.com>
2018-05-09 02:38:23 -0600

    cleaned extra line code

--- nonRealTime/RealTime.py
+++ nonRealTime/RealTime.py
@@ -14,19 +14,9 @@ from collections import deque
 import threading
 import logging
 
-##timeEntry = False
-##t0 = time.time()
-#initialize time, just to get into main's scope
-
 import RPi.GPIO as GPIO
 
 import sys
-
-##lower_time_factor = 0.85#this may not be doing anything,
-#move to constructor?
-##first_lowered = False
-
-##screen_length = 0.97
 
 delay_time_down = .04
 delay_time_release = .01
@@ -53,136 +43,102 @@ def setup():
     GPIO.output(LedPin4, GPIO.LOW) # Set LedPin high(+3.3V) to turn on led
 
 def worker(num,Lane):
-##    print num
-
-##    if num 
-
-    
-##    time.sleep(2.75*num*lower_time_factor)
     time.sleep(3.0*num)
 
-##    time.sleep(.26)
-
     if Lane == 1:
-##        cv2.circle(frame,(L1,H2), 10, RED, -1)
-##        print "tap lane 1"
-        GPIO.output(LedPin, GPIO.HIGH)  # led on
+        GPIO.output(LedPin, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin, GPIO.LOW)  # led on
+        GPIO.output(LedPin, GPIO.LOW)  
         time.sleep(delay_time_release)
 
     elif Lane == 2:
-##        cv2.circle(frame,(L2,H2), 10, RED, -1)
-##        print "tap lane 2"
-        GPIO.output(LedPin2, GPIO.HIGH)  # led on
+        GPIO.output(LedPin2, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin2, GPIO.LOW)  # led on
+        GPIO.output(LedPin2, GPIO.LOW)  
         time.sleep(delay_time_release)
     elif Lane == 3:
-##        cv2.circle(frame,(L3,H2), 10, RED, -1)
-##        print "tap lane 3"
-        GPIO.output(LedPin3, GPIO.HIGH)  # led on
+        GPIO.output(LedPin3, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin3, GPIO.LOW)  # led on
+        GPIO.output(LedPin3, GPIO.LOW)  
         time.sleep(delay_time_release)
         
     elif Lane == 4:
-##        cv2.circle(frame,(L4,H2), 10, RED, -1)
-##        print "tap lane 4"
-        GPIO.output(LedPin4, GPIO.HIGH)  # led on
+        GPIO.output(LedPin4, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin4, GPIO.LOW)  # led on
+        GPIO.output(LedPin4, GPIO.LOW)  
         time.sleep(delay_time_release)
 
 def worker_simple(num,Lane):
-    time.sleep(num)#do not multiply by lower time factor
-
-##    time.sleep(.26)
+    time.sleep(num) # do not multiply by lower time factor
 
     if Lane == 1:
-##        cv2.circle(frame,(L1,H2), 10, RED, -1)
         print "tap lane 1"
-        GPIO.output(LedPin, GPIO.HIGH)  # led on
+        GPIO.output(LedPin, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin, GPIO.LOW)  # led on
+        GPIO.output(LedPin, GPIO.LOW)  
         time.sleep(delay_time_release)
 
     elif Lane == 2:
-##        cv2.circle(frame,(L2,H2), 10, RED, -1)
         print "tap lane 2"
-        GPIO.output(LedPin2, GPIO.HIGH)  # led on
+        GPIO.output(LedPin2, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin2, GPIO.LOW)  # led on
+        GPIO.output(LedPin2, GPIO.LOW)  
         time.sleep(delay_time_release)
     elif Lane == 3:
-##        cv2.circle(frame,(L3,H2), 10, RED, -1)
         print "tap lane 3"
-        GPIO.output(LedPin3, GPIO.HIGH)  # led on
+        GPIO.output(LedPin3, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin3, GPIO.LOW)  # led on
+        GPIO.output(LedPin3, GPIO.LOW)  
         time.sleep(delay_time_release)
         
     elif Lane == 4:
-##        cv2.circle(frame,(L4,H2), 10, RED, -1)
         print "tap lane 4"
-        GPIO.output(LedPin4, GPIO.HIGH)  # led on
+        GPIO.output(LedPin4, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin4, GPIO.LOW)  # led on
+        GPIO.output(LedPin4, GPIO.LOW)  
         time.sleep(delay_time_release)
 
 
 def doubleTapWorker(num,Lane,c):
-##    time.sleep(2.5* num * lower_time_factor)
     time.sleep(3.0* num)
 
-##    print Lane
-
-##    time.sleep(.26)
-##    print Lane
-
     if Lane == 1:
-##        print "tap tap lane 1"
-        GPIO.output(LedPin, GPIO.HIGH)  # led on
+        GPIO.output(LedPin, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin, GPIO.LOW)  # led on
+        GPIO.output(LedPin, GPIO.LOW)  
         time.sleep(delay_time_release)
         c.set_val(0)
 
     elif Lane == 2:
-####        cv2.circle(frame,(L2,H2), 10, RED, -1)
-##        print "tap tap lane 2"
-        GPIO.output(LedPin2, GPIO.HIGH)  # led on
+        GPIO.output(LedPin2, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin2, GPIO.LOW)  # led on
+        GPIO.output(LedPin2, GPIO.LOW)  
         time.sleep(delay_time_release)
         c.set_val(0)
+
     elif Lane == 3:
-##        cv2.circle(frame,(L3,H2), 10, RED, -1)
-##        print "tap tap lane 3"
-        GPIO.output(LedPin3, GPIO.HIGH)  # led on
+        GPIO.output(LedPin3, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin3, GPIO.LOW)  # led on
+        GPIO.output(LedPin3, GPIO.LOW)  
         time.sleep(delay_time_release)
         c.set_val(0)
         
     elif Lane == 4:
-##        cv2.circle(frame,(L4,H2), 10, RED, -1)
-##        print "tap tap lane 4"
-        GPIO.output(LedPin4, GPIO.HIGH)  # led on
+        GPIO.output(LedPin4, GPIO.HIGH)  
         time.sleep(delay_time_down)
 
-        GPIO.output(LedPin4, GPIO.LOW)  # led on
+        GPIO.output(LedPin4, GPIO.LOW)  
         time.sleep(delay_time_release)
         c.set_val(0)
 
@@ -192,13 +148,9 @@ class Counter(object):
         self.lock = threading.Lock()
         self.value = start #no doubles at start
     def set_val(self,val):
-##        logging.debug('Waiting for lock')
         self.lock.acquire()
         try:
-##            logging.debug('Acquired lock')
             self.value = val
-##            logging.debug('Waiting for worker threads')
-##            print self.value
         finally:
             self.lock.release()
             
@@ -216,14 +168,9 @@ class TimeMarker():
 
         self.LastLane = 0
 
-##        self.circular_queue = \
-##        deque(np.repeat(AVG_INIT,AVG_SIZE),maxlen=AVG_SIZE)
         self.circular_queue = deque([0.3,0.3,0.3,0.3,0.3,0.3,0.3], maxlen=AVG_SIZE)
 
         self.currentAVG = 0.3
-
-##        self.first_lowered = False
-##        self.second_lowered = False
         
         # need to put a "fake" .01 case at the end to prevent index out of bounds
         self.tier_arr = np.array([.17,.13,.11,.08,0])
@@ -240,24 +187,10 @@ class TimeMarker():
         return smas[0]
         
     def markTime(self,BlackLane,adjust):
-    ##    if timeEntryBool == True:
         if(BlackLane != self.LastLane) and BlackLane != 0:
             self.t1 = time.time()
-            #1.329 inches on calipers
 
             currentMeasure = (self.t1 - self.last_time)
-
-##            print currentMeasure
-
-##            if currentMeasure >= .31:
-##                currentMeasure = .31
-                
-            #lower time factor, only do this once.
-##            if (self.currentAVG <= self.tier_arr[self.tier_i]):
-##                print "lowering time factor to ",self.tier_i
-##                self.my_lower_time = self.Lower_arr[self.tier_i]
-##                self.tier_i += 1
-
 
             if adjust == 0:
                 t = threading.Thread\
@@ -269,11 +202,8 @@ class TimeMarker():
                 (target = worker, args=((self.currentAVG*.833)*self.my_lower_time,BlackLane,))
                 threads.append(t)
                 t.start()
-            # and currentMeasure < self.circular_queue[-1]
-            # abs((self.circular_queue[-1] - currentMeasure)) < .08
+
             if abs((self.circular_queue[-1] - currentMeasure)) < .2:
-##            self.circular_queue.append( 1.0 / (self.t1 - self.last_time) )
-##                print currentMeasure
                 if adjust == 0:
                     print currentMeasure
                     self.circular_queue.append( currentMeasure )
@@ -326,15 +256,11 @@ class TimeMarker():
 
     def try_tap(self,lane,counterObj):
         
-        if( counterObj.value == 0):# if not already currently double tapping lane #
-##            print lane
-
-##            self.doubleTap[lane] = 1 #might need thread locking
+        if( counterObj.value == 0): # if not already currently double tapping lane #
             counterObj.set_val(1)
             a = threading.Thread\
             (target = doubleTapWorker, args=(self.currentAVG*self.my_lower_time,lane,counterObj,))
-##            threads.append(a)
-            a.start() # note this can still cause more than one threads named "a"
+            a.start()
         else:
             pass
 
@@ -345,7 +271,6 @@ class TimeMarker():
         t.start()
 
         
-
 setup() # setup GPIO's
 
 PianoCalibration = np.load('PianoCalibration.npz')
@@ -402,12 +327,6 @@ for img in camera.capture_continuous(rawCapture, format="bgr", use_video_port=Tr
     elif(black_lane_h3 == 3): timer.simple_tap(black_lane_h3,.33)
     elif(black_lane_h3 == 4): timer.simple_tap(black_lane_h3,.33)
 
-    
-
-##    cv2.line(frame,(0,H1),(240,H1),VIOLET,1)
-            
-
-##    cv2.imshow('Frame',frame)
 
     # clear the stream in preparation for the next frame
     rawCapture.truncate(0)
@@ -417,9 +336,6 @@ for img in camera.capture_continuous(rawCapture, format="bgr", use_video_port=Tr
 
     break #only do this loop once
 
-##time.sleep(5)
-##sys.exit() #for now add a "breakpoint" here
-    
 
 for img in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 ##    print "currentAVG is :",timer.currentAVG
@@ -437,7 +353,6 @@ for img in camera.capture_continuous(rawCapture, format="bgr", use_video_port=Tr
                 
         if( black_lane_h3 == black_lane_hm ):
 ##            print "at least double detected"
-##            print "double in lane ",black_lane_h3
             if(black_lane_h3 == 1): timer.try_tap(black_lane_hm,counter1)
             elif(black_lane_h3 == 2): timer.try_tap(black_lane_hm,counter2)
             elif(black_lane_h3 == 3): timer.try_tap(black_lane_hm,counter3)
@@ -450,17 +365,7 @@ for img in camera.capture_continuous(rawCapture, format="bgr", use_video_port=Tr
                 if(black_lane_h2 == 1): timer.try_tap(black_lane_hm,triplecounter1)
                 elif(black_lane_h2 == 2): timer.try_tap(black_lane_hm,triplecounter2)
                 elif(black_lane_h2 == 3): timer.try_tap(black_lane_hm,triplecounter3)
-                elif(black_lane_h2 == 4): timer.try_tap(black_lane_hm,triplecounter4)##
-    ##            if (black_lane_h2 == black_lane_hm):
-    ##                print "triple detected in lane ",black_lane_h2
-
-            
-                    
-    ##        timer.markTime( timer.checkHeight(HighMid) )
-            # and don't pass in timer anymore
-            
-##    cv2.line(frame,(0,H1),(240,H1),VIOLET,1)
-            
+                elif(black_lane_h2 == 4): timer.try_tap(black_lane_hm,triplecounter4)
 
     cv2.imshow('Frame',frame)
 
@@ -471,9 +376,6 @@ for img in camera.capture_continuous(rawCapture, format="bgr", use_video_port=Tr
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
-
-
- 
 
 GPIO.output(LedPin, GPIO.LOW)   # led off
 GPIO.output(LedPin2, GPIO.LOW)   # led off
